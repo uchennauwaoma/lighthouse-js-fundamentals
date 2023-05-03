32e2187e8a64f2771fc0d5d60bb20e0c0f2b95d0
uchennauwaoma <uwaoma.uchenna@gmail.com>
2023-05-03 09:32:36 -0600

    added more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -9,3 +9,26 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside");
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside is not such a good idea");
+}
+
+const raining = true;
+const cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine");
+}
+
+console.log("Now you're ready to go outside!");
+
+if (!raining) {
+  console.log("leave your umbrella at home!");
+}
